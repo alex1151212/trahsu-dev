b2b23968217651c95b8b6625e16931edf44253f6
alex1151212 <alex1151212@gmail.com>
2024-07-22 12:33:42 +0800

    Deploy website - based on 0e754948415845fec004ec78f72dad5bb70d3ec2

---
+++ assets/js/bc4ec8db.0423ef24.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunktrahsu_dev=self.webpackChunktrahsu_dev||[]).push([[28],{5603:e=>{e.exports=JSON.parse('{"title":"Web Dev","description":"Web Dev","slug":"/category/web-dev","permalink":"/docs/category/web-dev","navigation":{"previous":{"title":"Trahsu Note","permalink":"/docs/intro"},"next":{"title":"[Web]HTML","permalink":"/docs/web-dev/HTML"}}}')}}]);
